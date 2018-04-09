b6611b0ade10934d7b80a318be283c1207605a03
tullosjr <tullosjr@gmail.com>
2018-04-08 22:15:44 -0700

    Fix bug-7
    changed Item.key to private

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,1 +1,1 @@
-package main.java;public final class Item {        public int key;        //constructor        public Item(int keyValue) {                 key = keyValue;        }        // further components and methods}
+package main.java;public final class Item {        private  int key;        //constructor        public Item(int keyValue) {                 key = keyValue;        }        // further components and methods}
